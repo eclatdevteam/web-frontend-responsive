dd1d37afde2b77a855326cc0da0ec449d67bb3ee
1rubiz <1izekorruby@gmail.com>
2025-09-23 17:49:21 +0100

    new ui

--- app/(auth)/_components/login.tsx
+++ app/(auth)/_components/login.tsx
@@ -1,32 +1,42 @@
+'use client'
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardDescription, CardHeader } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
+import { ArrowLeft } from 'lucide-react'
 import Image from 'next/image'
 import React from 'react'
+import { useRouter } from 'next/navigation'
+import { Label } from '@/components/ui/label'
 
 function Login() {
+    const router = useRouter()
     return (
-        <Card className='w-full md:max-w-xl bg-ruby'>
+        <Card className='w-full md:max-w-xl bg-ruby shadow-none border-none'>
             <CardHeader className='flex items-center justify-center w-full'>
-                <Image src='/logo.png' alt='' width={300} height={300} className='w-32 h-16' />
+                <div className='w-full flex items-center relative justify-center mt-14 mb-5'>
+                    <ArrowLeft className='text-primary3 absolute top-1/4 left-0' onClick={()=> router.push('/auth')}/>
+                    <Image src='/logo.png' alt='' height={200} width={200} className='h-16 w-32'/>
+                </div>
             </CardHeader>
             <CardDescription className='text-center'>
-                <h1 className='text-[#167396] font-semibold text-2xl'>Welcome back</h1>
-                <p className='text-xs text-[#212121]'>Welcome back please login with the required information </p>
+                <h1 className='text-[#167396] font-semibold text-2xl mb-3'>Welcome back</h1>
+                <p className='text-xs text-[#212121]'>Let&apos; pick it up from where you left. </p>
             </CardDescription>
-            <CardContent className='space-y-6'>
-                <Input className='placeholder:text-xs h-10 lg:h-14' placeholder='Email' />
-                <Input className='placeholder:text-xs h-10 lg:h-14' placeholder='Password' />
+            <CardContent className='space-y-6 px-4 shadow-none'>
+                <Input className='placeholder:text-xs h-10 lg:h-14' placeholder='Email address pr phone number' />
+                <Label>
+                    <Input className='placeholder:text-xs h-10 lg:h-14' placeholder='Enter your password' />
+                </Label>
                 <div className='w-full flex items-center justify-end'>
                     <p className='text-xs text-[#E53935]'>Forget Password ?</p>
                 </div>
                 <div className='grid grid-cols-8 items-center justify-between gap-4'>
-                    <Button className='col-span-6 bg-[#167396] h-10 lg:h-14 text-white w-full'>Login</Button>
-                    <div className='col-span-2 flex items-center justify-end'>
+                    <Button className='col-span-8 bg-[#167396] h-10 lg:h-14 text-white w-full'>Login</Button>
+                    {/* <div className='col-span-2 flex items-center justify-end'>
                         <div className='h-10 lg:h-14 w-10 lg:w-14 flex items-center justify-center rounded-md col-span-1 bg-transparent border-2'>
                             <Image className='w-6 lg:w-9 h-6 lg:h-9' src='/googleIcon.svg' alt='' width={300} height={300} />
                         </div>
-                    </div>
+                    </div> */}
                 </div>
             </CardContent>
         </Card>
---
+++ app/auth/login/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+import Login from "@/app/(auth)/_components/login";
+import React from 'react'
+
+function page() {
+  return <Login/>
+}
+
+export default page
